261a2ab2bcf1d00113be648d8d9d8738f8247582
BenjaminC62 <benjamincornet62130@gmail.com>
2025-10-12 23:25:07 +0200

    ajout de la base du front

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,23 @@
+const Home = () => {
+    return (
+        <>
+            <header>
+                <nav>
+                    test
+                </nav>
+            </header>
+
+
+            <main>
+                <p className="">Welcome!</p>
+            </main>
+
+
+            <footer>
+                <p>&copy; 2025 Quickpool</p>
+            </footer>
+        </>
+    )
+}
+
+export default Home;
